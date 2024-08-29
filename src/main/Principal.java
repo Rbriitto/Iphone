e2645b5b943d6e7893c662036f8fb4303d3aee4f
Rbriitto <rodrigobritto.web@gmail.com>
2024-08-29 12:51:30 -0300

    save2

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,16 +1,24 @@
 
 package main;
 
-
-
+import iphone.Iphone;
 
 public class Principal {
-    public void static main (String[] args){
+    public  static void main(String[] args){
 
         Iphone celular = new Iphone ();
         
         celular.selecionarMusica(" Beat it - Michael Jackson");
         celular.tocar();
         celular.pausar();
+
+        celular.exibirPagina("www.google.com");
+        celular.adicionddarNovaAba();
+        celular.atualizarPagina();
+
+        celular.ligar("21999999990");
+        celular.atender();
+        celular.iniciarCorreioVoz();
+        
 
     }}
