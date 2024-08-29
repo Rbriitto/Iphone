93910e339330db9a84ffced95689e2e64defa5f8
Rbriitt <rodrigobritto.web@gmail.com>
2024-08-29 06:30:38 -0300

    save3

---
+++ src/appmusica/Musicas.java
@@ -0,0 +1,17 @@
+package appmusica;
+
+public class Musicas {
+
+    public void setSelecionarMusica(String musica) {
+
+    }
+
+    public void tocar() {
+
+    }
+
+    public void pausar() {
+
+    }
+
+}
---
+++ src/navegador/Internet.java
@@ -0,0 +1,15 @@
+package navegador;
+
+public class Internet {
+
+    public void exibirPagina(String url) {
+     System.out.println("Pagina:" + url);
+    }
+
+    public void adicionarNovaAba() {
+    }
+
+    public void atualizarPagina() {
+    }
+
+}
